417695b8342cd7891bbbb5a8d913403a81c7b2e2
Jeongmin <tdcian71@gmail.com>
2021-04-01 09:43:04 +0900

    Math Stuff 파일 추가

---
+++ TestTest/MathStuff.swift
@@ -0,0 +1,23 @@
+//
+//  MathStuff.swift
+//  TestTest
+//
+//  Created by APPLE on 2021/04/01.
+//
+
+import Foundation
+
+class MathStuff {
+    
+    func addNumbers(x: Int, y: Int) -> Int {
+        return x + y
+    }
+    
+    func multipleNumbers(x: Int, y: Int) -> Int {
+        return x * y
+    }
+    
+    func divideNumbers(x: Int, y: Int) -> Int {
+        return x / y
+    }
+}
